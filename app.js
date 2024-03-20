3d57bb4fa27794be13fcd2ae8c7b455ded9b45de
Ange290 <uange209@gmail.com>
2024-03-20 12:16:32 +0200

    Task Manager Back -end Project

--- app.js
+++ app.js
@@ -11,11 +11,7 @@ app.use(express.json());
  });
 
 app.use('/api/get/tasks', router)
-//app.get('/api/get/tasks') - get all tasks
-//app.post('/api/post/tasks') - create new tasks
-//app.get('/api/get/tasks/:id') - get single task
-//app.patch('/api/patch/tasks/:id') - update task
-//app.delete('/api/delete/tasks/:id') - delete task
+
 const port = 3000;
 
 const start = async()=>{
